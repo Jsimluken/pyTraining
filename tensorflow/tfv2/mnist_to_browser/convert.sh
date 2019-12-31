c4cb20cd71b7bd0d82e0ca378bed1eef32d92f62
Michael Laurents <simonsluke134@gmail.com>
2019-12-31 14:02:58 +0900

    add graph model and deep into model json

--- tensorflow/tfv2/mnist_to_browser/convert.sh
+++ tensorflow/tfv2/mnist_to_browser/convert.sh
@@ -2,6 +2,6 @@
 docker run -it --rm -v `pwd`/model:/opt/model \
     jsimluken/tfjs tensorflowjs_converter \
     --input_format=keras \
-    --output_format=tfjs_layers_model \
+    --output_format=tfjs_graph_model \
     /opt/model/tiny_mnist.h5\
-    /opt/model/web_model
+    /opt/model/web_model2
